be487bd514eac77cbaef7cb3d340aa7885174d2c
pie <khanhtnfptdn@gmail.com>
2022-09-30 17:15:39 +0700

    add babel plugin module

--- src/Components/header/Header.js
+++ src/Components/header/Header.js
@@ -1,19 +1,103 @@
-import React from "react";
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
 import { Link } from "react-router-dom";
+import MenuItem from "@mui/material/MenuItem";
+import {
+  AppBar,
+  Container,
+  IconButton,
+  Menu,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import MovieOutlinedIcon from "@mui/icons-material/MovieOutlined";
+import styles from "./Header.module.css";
+
+const categories = [
+  "Tu Tiên",
+  "Luyện Cấp",
+  "Trùng sinh",
+  "Xuyên không",
+  "Hiện đại",
+];
 
 function Header() {
+  const [anchorElNav, setAnchorElNav] = useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
   return (
     <div className="header">
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-        </ul>
-      </nav>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="fixed" className={styles.navbar}>
+          <Container maxWidth="xl">
+            <Toolbar disableGutters>
+              <MovieOutlinedIcon
+                sx={{ display: { xs: "none", md: "flex" }, mr: 1 }}
+              />
+              <Typography
+                variant="h6"
+                noWrap
+                component="a"
+                href="/"
+                sx={{
+                  mr: 2,
+                  display: { xs: "none", md: "flex" },
+                  fontFamily: "monospace",
+                  fontWeight: 700,
+                  letterSpacing: ".3rem",
+                  color: "inherit",
+                  textDecoration: "none",
+                }}
+              >
+                Relax 321
+              </Typography>
+
+              <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+                <IconButton
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleOpenNavMenu}
+                  color="inherit"
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorElNav}
+                  anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "left",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "left",
+                  }}
+                  open={Boolean(anchorElNav)}
+                  onClose={handleCloseNavMenu}
+                  sx={{
+                    display: { xs: "block", md: "none" },
+                  }}
+                >
+                  {categories.map((cate) => (
+                    <MenuItem key={cate} onClick={handleCloseNavMenu}>
+                      <Typography textAlign="center">{cate}</Typography>
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </Box>
+            </Toolbar>
+          </Container>
+        </AppBar>
+      </Box>
     </div>
   );
 }
